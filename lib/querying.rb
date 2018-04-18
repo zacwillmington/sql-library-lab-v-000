65108707cec190a37890fb8366b92aafb2c57220
Zac Willmington <zac.willmington@gmail.com>
2018-04-18 21:24:53 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,5 +31,8 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT *
+ FROM characters
+outer JOIN character_books
+ ON characters.id = character_books.character_id;"
 end
