b939cf472e832e8469387baf74e620dd5af72765
Zac Willmington <zac.willmington@gmail.com>
2018-04-18 21:57:19 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,8 @@ FROM characters
 inner join series
 ON characters.series_id = series.id
 WHERE characters.species = 'human'
-ORDER BY series.title ASC LIMIT 1;"
+group by series.title
+ORDER BY sum(characters.species = 'human') DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
