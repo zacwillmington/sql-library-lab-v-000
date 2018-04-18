7181737406333225bc58ee3931a5d9dca042973c
Zac Willmington <zac.willmington@gmail.com>
2018-04-18 20:54:33 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,22 +1,37 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year
+    FROM books
+    WHERE books.series_id = 1
+    ORDER BY books.year ASC;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto
+    FROM characters
+    ORDER BY length (characters.motto) DESC LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species) AS species_count
+  FROM characters
+  GROUP BY characters.species
+  ORDER BY species_count DESC LIMIT 1; "
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+FROM series
+LEFT JOIN authors, subgenres
+ON series.author_id = authors.id AND series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title, COUNT(characters.species) AS species_count
+FROM characters
+INNER JOIN series
+ON characters.series_id = series.id
+GROUP BY characters.species = 'human' LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
