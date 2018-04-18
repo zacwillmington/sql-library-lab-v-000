f3ba7621b9c67b02bc76f6c86294eecfeb1be4f9
Zac Willmington <zac.willmington@gmail.com>
2018-04-18 21:38:54 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,9 +31,11 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name , COUNT(character_books.book_id)
- FROM characters
-inner JOIN character_books
- ON characters.id = character_books.character_id
- GROUP BY characters.name;"
+    "SELECT characters.name , COUNT(character_books.book_id) 
+    FROM characters
+   inner JOIN character_books
+    ON characters.id = character_books.character_id
+    GROUP BY characters.name 
+    ORDER BY COUNT(character_books.book_id)  DESC, characters.name ASC
+   ;"
 end
